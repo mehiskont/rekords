af11c428cc0e642902e7aa9ea805e79bf5749079
Mehis Kont <mehiskont@gmail.com>
2025-03-22 08:34:33 +0200

    navbar paddings

--- components/nav-bar.tsx
+++ components/nav-bar.tsx
@@ -35,7 +35,7 @@ export function NavBar() {
 
   return (
     <nav className="sticky top-0 z-50 bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60 border-b">
-      <div className="container max-w-6xl mx-auto px-4">
+      <div className="container mx-auto">
         <div className="flex h-16 items-center justify-between">
           <div className="flex items-center gap-8">
             <Link
