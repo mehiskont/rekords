88c9a449ac2f2e1743c773947dd47b11ab8d0a28
Mehis Kont <mehiskont@gmail.com>
2025-04-11 10:59:00 +0300

    signin animation

--- app/auth/signin/page.tsx
+++ app/auth/signin/page.tsx
@@ -1,5 +1,11 @@
 import type { Metadata } from "next"
 import { SignInForm } from "@/components/auth/sign-in-form"
+import dynamic from 'next/dynamic'
+
+const Background = dynamic(() => import('@/components/auth/background-animation'), {
+  ssr: false,
+  loading: () => <div className="h-full w-full animate-pulse bg-zinc-700" />
+})
 
 export const metadata: Metadata = {
   title: "Sign In | Plastik Record Store",
@@ -11,20 +17,8 @@ export default function SignInPage() {
     <div className="container relative min-h-screen flex-col items-center justify-center grid lg:max-w-none lg:grid-cols-2 lg:px-0">
       <div className="relative hidden h-full flex-col bg-muted p-10 text-white dark:border-r lg:flex">
         <div className="absolute inset-0 bg-zinc-900" />
-        <div className="relative z-20 flex items-center text-lg font-medium">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 24 24"
-            fill="none"
-            stroke="currentColor"
-            strokeWidth="2"
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            className="mr-2 h-6 w-6"
-          >
-            <path d="M15 22v-4a4.8 4.8 0 0 0-1-3.5c3 0 6-2 6-5.5.08-1.25-.27-2.48-1-3.5.28-1.15.28-2.35 0-3.5 0 0-1 0-3 1.5-2.64-.5-5.36-.5-8 0C6 2 5 2 5 2c-.3 1.15-.3 2.35 0 3.5A5.403 5.403 0 0 0 4 9c0 3.5 3 5.5 6 5.5-.39.49-.68 1.05-.85 1.65-.17.6-.22 1.23-.15 1.85v4" />
-          </svg>
-          Plastik Record Store
+        <div className="relative z-20 flex h-full w-full items-center justify-center">
+          <Background />
         </div>
       </div>
       <div className="lg:p-8">
